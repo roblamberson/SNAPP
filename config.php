1c43fa7aedc2c073b7f4705baf1948a9a206d1c7
Rob Lamberson <37662543+roblamberson@users.noreply.github.com>
2019-05-10 14:41:17 -0400

    Initial upload
    Just what I'm working with at the moment.  Don't bother giving this any attention at this point.  It's almost entirely unusable, for now.

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+	'db' => array(	'database_type' => 'mysql',
+					'database_name' => 'delivery',
+					'server' 		=> 'localhost',
+					'username' 		=> 'burrito',
+					'password' 		=> '2.Boobies'),
+					
+	'php_dir'	=> '../php', // '../php' for one below webroot or '/php'
+							// if having to keep in same dir as this file
+	'data'		=> '../html/data',  // relative location of app's inner data files
+	'uploads'	=> 'files/cloud/data/albalux/files',
+	'registration'	=> array(	'required' => array("email", "username"))
+	);
+
+	
+	
+	
+?>
